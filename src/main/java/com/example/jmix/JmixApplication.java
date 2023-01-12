e94a169c8f4a0f4899a61c6cbcae5449a4b58550
user <chynara.mk4@gmail.com>
2023-01-12 21:53:48 +0600

    Initial commit

---
+++ src/main/java/com/example/jmix/JmixApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jmix/JmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
